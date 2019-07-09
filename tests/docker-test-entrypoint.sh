80fda0b21631ac182c88a10aa5ee2e3cde55d504
Tomas Heger <tomas.heger@qoretechnologies.com>
2019-07-09 07:27:44 +0000

    refs #3107 reverted to use SFTP image instead of rippy, make sure interface directories are created for test

--- tests/docker-test-entrypoint.sh
+++ tests/docker-test-entrypoint.sh
@@ -5,12 +5,17 @@ set -e
 . /tmp/env.sh
 
 launch() {
+    # setup image
     $WORKDIR/qorus/test/docker_test/init.sh
     ./install.sh
+    # run qorus
     gosu qorus:qorus $WORKDIR/qorus/test/docker_test/launch_qorus.sh
+    # oload tests
     gosu qorus:qorus oload -Rlv tests/SftpJobModule/*.q*
     #gosu qorus:qorus oload -Rlv tests/SftpWorkflowModule/*.q*
+    # run tests
     gosu qorus:qorus qore tests/SftpJobModule/SftpJobModule.qtest -vv
+    # turn off qorus
     gosu qorus:qorus $WORKDIR/qorus/test/docker_test/turn_off_qorus.sh
 }
 
