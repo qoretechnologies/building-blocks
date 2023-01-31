13cff0442bdf43eb79a07d2f2ebc0d83c244a273
Rishabh Malik <rishabhmalik759@hotmail.com>
2023-01-31 13:59:24 +0100

    Updated github CI

--- test/docker/setup.sh
+++ test/docker/setup.sh
@@ -94,8 +94,8 @@ echo --- downloading Kafka
 cd /opt
 wget https://downloads.apache.org/kafka/3.3.2/kafka_2.12-3.3.2.tgz
 echo --- installing Kafka
-tar xvf kafka_2.13-3.3.1.tgz
-cd kafka_2.13-3.3.1
+tar xvf kafka_2.12-3.3.2.tgz
+cd kafka_2.12-3.3.2
 echo --- starting Kafka
 ./bin/zookeeper-server-start.sh config/zookeeper.properties &
 ./bin/kafka-server-start.sh config/server.properties &
