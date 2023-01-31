a86eb7f28a423eab762335152ff9c753a8a16307
Rishabh Malik <rishabhmalik759@hotmail.com>
2023-01-31 12:37:17 +0100

    Updated github CI

--- test/docker/setup.sh
+++ test/docker/setup.sh
@@ -7,7 +7,7 @@ set -x
 
 start_postgres() {
     docker run --name=postgres --network=host -e POSTGRES_PASSWORD=omq -e TZ=Europe/Prague -e PGTZ=Europe/Prague -d postgres:15
-    docker -ps
+    docker ps
     # wait for PostgreSQL server to start
     printf "waiting on PostgreSQL server: "
     waited=0
