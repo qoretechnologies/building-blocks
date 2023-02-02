3bcd2196cd1a6c3169967a418235fb9f4bee0dde
Rishabh Malik <rishabhmalik759@hotmail.com>
2023-02-02 11:59:37 +0100

    Updated github CI

--- test/docker/setup.sh
+++ test/docker/setup.sh
@@ -57,10 +57,10 @@ wait_for_qorus() {
     done
 }
 
-/opt/qorus/bin/entrypoint.sh & wait_for_qorus
 
 start_postgres
 
+/opt/qorus/bin/entrypoint.sh & wait_for_qorus
 
 # setup Kafka
 echo --- downloading Kafka
