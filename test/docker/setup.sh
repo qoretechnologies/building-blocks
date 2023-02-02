f6e9a6bc05b7e154c477411c044a38ed55859431
Rishabh Malik <rishabhmalik759@hotmail.com>
2023-02-02 13:10:33 +0100

    Updated github CI

--- test/docker/setup.sh
+++ test/docker/setup.sh
@@ -6,13 +6,12 @@ set -x
 . /opt/qorus/bin/env.sh
 
 start_postgres() {
-    # docker run --name=postgres --network=host -e POSTGRES_PASSWORD=omq -e TZ=Europe/Prague -e PGTZ=Europe/Prague -d postgres:15
 
     # wait for PostgreSQL server to start
     printf "waiting on PostgreSQL server: "
     waited=0
     while true; do
-        ver=$(qore -l Util -ne 'try { printf("%s", (new Datasource("pgsql:postgres/omq@postgres%localhost:5432")).getServerVersion()); } catch (hash<ExceptionInfo> ex) { printf("%s\n", get_exception_string(ex)); }')
+        ver=$(qore -l Util -ne 'try { printf("%s", (new Datasource("pgsql:postgres/omq@postgres%localhost:5432")).selectRow("select version()").version); } catch (hash<ExceptionInfo> ex) { printf("%s\n", get_exception_string(ex)); }')
         if [ -n "$ver" ]; then
             echo ": started server version $ver"
             break
@@ -20,7 +19,7 @@ start_postgres() {
 
         # 30 second time limit
         if [ $waited -eq 30 ]; then
-            echo && echo "Waited too long for PostgreSQL to start; aborting build."
+            echo && echo "Waited too long to connect with PostgreSQL; aborting build."
             exit 1
         fi
         printf .
@@ -29,17 +28,37 @@ start_postgres() {
         waited=$((waited + 1))
     done
 
-    export OMQ_DB_USER=postgres
-    export OMQ_DB_PASS=omq
-    export OMQ_DB_NAME=postgres
-    export OMQ_DB_HOST=postgres
-    export OMQ_SYSTEMDB=pgsql:${OMQ_DB_USER}/${OMQ_DB_PASS}@${OMQ_DB_NAME}%${OMQ_DB_HOST}
-
     # make sure we can access the DB
     qore -nX "(new Datasource(\"${OMQ_SYSTEMDB}\")).getServerVersion()"
 }
 
+wait_for_qorus() {
+    # wait for Qorus to start
+    printf "waiting on Qorus to start:"
+    waited=0
+    while true; do
+        pid=$(qrest system/pid 2>/dev/null)
+        if [ $? -eq 0 ]; then
+            echo " Qorus started"
+            break
+        fi
+
+        # 30 second time limit
+        if [ $waited -eq 30 ]; then
+            echo && echo "Waited too long to connect to Qorus; aborting build."
+            exit 1
+        fi
+        printf .
+        # sleep for 1 second
+        sleep 1
+        waited=$((waited + 1))
+    done
+}
+
 start_postgres
+
+/opt/qorus/bin/entrypoint.sh &
+wait_for_qorus
 
 # setup Kafka
 echo --- downloading Kafka
